a12841c19a5196df19c6d91a82587e365654f148
PJH <thesn515@gmail.com>
2021-04-08 22:20:43 +0900

    Initial commit

---
+++ app/src/main/java/pjh/mjc/ex11_01/MainActivity.java
@@ -0,0 +1,22 @@
+package pjh.mjc.ex11_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+
+public class MainActivity extends AppCompatActivity {
+    final String[] mid = {"히어로즈", "24시", "로스트", "로스트룸", "스몰별", "탐정몽크",
+            "빅뱅이론", "프렌즈", "덱스터", "글리", "가쉽걸", "테이큰", "슈퍼내추럴", "브이"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("리스트뷰 테스트");
+
+        ListView listView = (ListView)findViewById(R.id.listView1);
+        MyListAdapter adapter = new MyListAdapter(this);
+        listView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/pjh/mjc/ex11_01/MyListAdapter.java
@@ -0,0 +1,46 @@
+package pjh.mjc.ex11_01;
+
+import android.app.Activity;
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+public class MyListAdapter extends BaseAdapter {
+    Context context;
+    String[] title;
+    public MyListAdapter(Context _context) {
+        context = _context; title = ((MainActivity)context).mid;
+    }
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.custom_adapter, parent, false);
+
+        TextView text1, text2;
+        text1 = (TextView) convertView.findViewById(R.id.tv1);
+        text2 = (TextView) convertView.findViewById(R.id.tv2);
+        text1.setText(title[position]);
+        text2.setText(title[position] + "의 내용");
+        return convertView;
+    }
+}
